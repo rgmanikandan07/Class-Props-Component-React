edc819c3122169d20853761007154c5fcb46f217
rgmanikandan07 <rgmanimani0707@gmail.com>
2022-10-19 20:06:05 +0530

    add new file

--- components-react-js/src/index.js
+++ components-react-js/src/index.js
@@ -4,31 +4,54 @@ import './index.css';
 import App from './App';
 
 
+// Class // 
+
 // function Main(){
 //   return <div>Manikandan</div>
 // }
-class Second extends React.Component{
-  render(){
-      return <div> second </div>
-  }
+
+// class Second extends React.Component{
+//   render(){
+//       return <div> second </div>
+//   }
+// }
+
+// class First extends React.Component{
+//   render(){
+//       return <div> first <Second /></div>
+//   }
+// }
+
+// class Main extends React.Component{
+//   render(){
+//       return <div> Main <First /></div>
+//   }
+// }
+
+// Props //
+
+function First(props) {
+  return (
+    <div>Heyyy {props.name}</div>
+  );
 }
 
-class First extends React.Component{
-  render(){
-      return <div> first <Second /></div>
-  }
-}
-
-class Main extends React.Component{
-  render(){
-      return <div> Main <First /></div>
-  }
+function SampleFun(props) {
+  console.log("props", props);
+  return <div>function {props.name} <First name={props.name}/></div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
-);
+root.render(<SampleFun name="mani" age="21" />);
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+        // <App />
+//   </React.StrictMode>
+// );
 
